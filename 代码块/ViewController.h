3376890e7c1135865607ef65c48be0f09cc5763d
Gate <luchangxiang1@163.com>
2015-09-02 14:59:58 +0800

    Initial Commit

---
+++ 代码块/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  代码块
+//
+//  Created by Gate on 15/9/2.
+//  Copyright (c) 2015年 Gate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 代码块/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  代码块
+//
+//  Created by Gate on 15/9/2.
+//  Copyright (c) 2015年 Gate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
